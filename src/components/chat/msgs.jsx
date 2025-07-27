3f18745bb481f5b52c5f8be74324adbfd5ac390b
Jothin-kumar <contact@jothin.tech>
2025-07-27 13:39:56 +0530

    use long polling to fetch messages (2 secs interval)

--- src/components/chat/msgs.jsx
+++ src/components/chat/msgs.jsx
@@ -6,19 +6,25 @@ export default function Messages({currentDialog}) {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    getChats(currentDialog)
+    const interval = setInterval(() => {      
+      getChats(currentDialog)
       .then(data => {
-        data.reverse()
         setMessages(data.filter(msg => msg.text && msg.text.trim() !== ""))
       })
       .catch(error => console.error("Error fetching chats:", error));
+    }, 2000);
+
+    return () => {
+      clearInterval(interval)
+      setMessages([])
+    }
   }, [currentDialog]);
 
   return (
-    <div style={{ padding: '10px', overflowY: 'scroll', height: '100%' }}>
-        {messages.map(msg => 
+    <div style={{ padding: '10px', overflowY: 'scroll', height: '100%', display: 'flex', flexDirection: 'column-reverse' }}>
+        {messages.length > 0 ? messages.map(msg => 
           <ChainMessages key={msg.id} name={msg.senderName} image={`http://localhost:8000/logos/${msg.senderId}.jpg`} messages={[msg.text]} />
-        )}
+        ) : <p>Loading...</p>}
     </div>
   );
 }
